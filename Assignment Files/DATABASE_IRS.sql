72c8aec8bc0f0e7f97b910826d59c9bfed171a75
Twunt <bpratten_09@hotmail.com>
2015-11-03 11:51:57 +1100

    Issue Bean
    Added Issue bean, has setters and getters for each value of the issue
    table.

--- Assignment Files/DATABASE_IRS.sql
+++ Assignment Files/DATABASE_IRS.sql
@@ -1,7 +1,3 @@
--- DROP DATABASE IRS;
-CREATE DATABASE IRS;
-
-use IRS;
 
 CREATE TABLE irsUser(
 userID VARCHAR(16) NOT NULL PRIMARY KEY,
@@ -11,7 +7,7 @@ role VARCHAR(30)
 );
 
 CREATE TABLE issue (
-issueID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+issueID INT NOT NULL PRIMARY KEY,
 submitterID VARCHAR(16) NOT NULL,
 staffID VARCHAR(16),
 state VARCHAR(10),
@@ -40,7 +36,7 @@ FOREIGN KEY (staffID) REFERENCES irsUser(userID) ON UPDATE CASCADE
 );
 
 CREATE TABLE userComment (
-commentID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+commentID INT NOT NULL PRIMARY KEY,
 userID VARCHAR(16) NOT NULL,
 issueID int NOT NULL,
 commentBody text,
@@ -51,7 +47,7 @@ FOREIGN KEY (issueID) REFERENCES issue(issueID) ON UPDATE CASCADE
 );
 
 CREATE TABLE maintenance (
-maintenanceID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+maintenanceID INT NOT NULL PRIMARY KEY,
 message text,
 startTime TIMESTAMP,
 endTime TIMESTAMP
@@ -64,39 +60,33 @@ insert into irsUser VALUES("AFaulds","Andrew Faulds","Thrillho","staff");
 insert into irsUser VALUES("NBrown","Nick Brown","nicksPassword","user");
 insert into irsUser VALUES("CDodd","Connor Dodd","connman","user");
 
-insert into userComment VALUES("Rbille",1,"Try turning it off and back on again","2015-10-26 11:00:00");
-insert into userComment VALUES(,,,);
+insert into userComment VALUES(1, "Rbille",1,"Try turning it off and back on again","2015-10-26 11:00:00");
 
 --Network Entries 
-insert into issue VALUES("LGulliver","RBille","inProgress","My computer can't find any available networks","No Network Found","network","cantConnect","common room",false,true,false,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
-insert into issue VALUES("BPrattern",NULL,"new","wep pages load very slowly","slow internet","network","speed","common room",true,true,false,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
-insert into issue VALUES("NBrown","AFaulds","kBase","whilst loading web pages, my internet cuts out","dropouts loading web pages","network","dropouts","NS117",true,true,true,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
-insert into issue VALUES("CDodd","AFaulds","inProgress","on many web pages, random popups come up and disrupt my work","getting popups","network","other","CR043",true,true,false,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+insert into issue VALUES(1, "LGulliver","RBille","inProgress","My computer can't find any available networks","No Network Found","network","cantConnect","common room",false,true,false,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+insert into issue VALUES(2,"BPrattern",NULL,"new","wep pages load very slowly","slow internet","network","speed","common room",true,true,false,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+insert into issue VALUES(3, "NBrown","AFaulds","kBase","whilst loading web pages, my internet cuts out","dropouts loading web pages","network","dropouts","NS117",true,true,true,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+insert into issue VALUES(4, "CDodd","AFaulds","inProgress","on many web pages, random popups come up and disrupt my work","getting popups","network","other","CR043",true,true,false,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
 
 --Software Entries
-insert into issue VALUES("NBrown","RBille","inProgress","It takes a long time to open any programs on my computer","Computer is running slowly","software","slowLoad","common room",NULL,NULL,NULL,NULL,"mac","MOS9.3.1","desktop","CR19","all","misc",NULL,NULL,NULL,NULL);
-insert into issue VALUES("BPrattern",NULL,"new","I can't open microsoft word","word won't open","software","wontLoad","common room",NULL,NULL,NULL,NULL,"pc","Windows 10","desktop","CR07","MS Word","2015",NULL,NULL,NULL,NULL);
-insert into issue VALUES("LGulliver","RBille","completed","Opening League of Legends opens DOTA2 as well. I DON'T WANT THIS","Running one program Opens another","software","other","LS043",NULL,NULL,NULL,NULL,"pc","Windows 7","laptop","Lewis PC","League of Legends","V4.21",NULL,NULL,NULL,NULL);
+insert into issue VALUES(5, "NBrown","RBille","inProgress","It takes a long time to open any programs on my computer","Computer is running slowly","software","slowLoad","common room",NULL,NULL,NULL,NULL,"mac","MOS9.3.1","desktop","CR19","all","misc",NULL,NULL,NULL,NULL);
+insert into issue VALUES(6, "BPrattern",NULL,"new","I can't open microsoft word","word won't open","software","wontLoad","common room",NULL,NULL,NULL,NULL,"pc","Windows 10","desktop","CR07","MS Word","2015",NULL,NULL,NULL,NULL);
+insert into issue VALUES(7, "LGulliver","RBille","completed","Opening League of Legends opens DOTA2 as well. I DON'T WANT THIS","Running one program Opens another","software","other","LS043",NULL,NULL,NULL,NULL,"pc","Windows 7","laptop","Lewis PC","League of Legends","V4.21",NULL,NULL,NULL,NULL);
 
 --Hardware Entries
-insert into issue VALUES("CDodd",NULL,"new","My computer won't start","dead computer","hardware","wontTurnOn","common room",NULL,NULL,NULL,"","","","","",NULL,NULL,NULL,NULL,NULL,NULL);
-insert into issue VALUES("CDodd","AFaulds","completed","my computer crashes whenever I run video games","computer crashes","hardware","blueScreens","common room",NULL,NULL,NULL,"","","","","",NULL,NULL,NULL,NULL,NULL,NULL);
-insert into issue VALUES("NBrown","AFaulds","kBase","my computer won't read CDs. I put the cd in the drive and it just doesnt recognise it","computer wont recognise CDs","hardware","diskDrive","NS107",NULL,NULL,NULL,"","","","","",NULL,NULL,NULL,NULL,NULL,NULL);
-insert into issue VALUES("BPrattern",NULL,"new","my headphones and microphone don't work. I plug them in but sound still plays through my monitor","headphones/microphone dont work","hardware","peripherals","NS247",NULL,NULL,NULL,"","","","","",NULL,NULL,NULL,NULL,NULL,NULL);
-insert into issue VALUES("LGulliver","RBille","resolved","my computer won't recognise my second hard drive. It's inserted into the port in the computer, but won't come up in My Computer","unrecognised hard drive","hardware","other","LS118",NULL,NULL,NULL,"","","","","",NULL,NULL,NULL,NULL,NULL,NULL);
+insert into issue VALUES(8, "CDodd",NULL,"new","My computer won't start","dead computer","hardware","wontTurnOn","common room",NULL,NULL,NULL,"","","","","",NULL,NULL,NULL,NULL,NULL,NULL);
+insert into issue VALUES(9, "CDodd","AFaulds","completed","my computer crashes whenever I run video games","computer crashes","hardware","blueScreens","common room",NULL,NULL,NULL,"","","","","",NULL,NULL,NULL,NULL,NULL,NULL);
+insert into issue VALUES(10, "NBrown","AFaulds","kBase","my computer won't read CDs. I put the cd in the drive and it just doesnt recognise it","computer wont recognise CDs","hardware","diskDrive","NS107",NULL,NULL,NULL,"","","","","",NULL,NULL,NULL,NULL,NULL,NULL);
+insert into issue VALUES(11, "BPrattern",NULL,"new","my headphones and microphone don't work. I plug them in but sound still plays through my monitor","headphones/microphone dont work","hardware","peripherals","NS247",NULL,NULL,NULL,"","","","","",NULL,NULL,NULL,NULL,NULL,NULL);
+insert into issue VALUES(12, "LGulliver","RBille","resolved","my computer won't recognise my second hard drive. It's inserted into the port in the computer, but won't come up in My Computer","unrecognised hard drive","hardware","other","LS118",NULL,NULL,NULL,"","","","","",NULL,NULL,NULL,NULL,NULL,NULL);
 
 --Email Entries 
-insert into issue VALUES("LGulliver","RBille","inProgress","an error pops up whenever i try to send an email","error on email send","email","cantSend",NULL,NULL,NULL,NULL,NULL,NULL,"",NULL,NULL,NULL,NULL,"","","",NULL);
-insert into issue VALUES("BPrattern",NULL,"new","my colleagues send me emails but I don't receive them. Nothing comes up in my inbox","not receiving emails","email","cantReceive",NULL,NULL,NULL,NULL,NULL,NULL,"",NULL,NULL,NULL,NULL,"","","",NULL);
-insert into issue VALUES("NBrown","RBille","kBase","I'm getting a lot of emails asking for my student username and password login","getting phishing emails","email","spamOrPhishing",NULL,NULL,NULL,NULL,NULL,NULL,"",NULL,NULL,NULL,NULL,"","","",NULL);
-insert into issue VALUES("NBrown","AFaulds","completed","Whenever I sign out of my email address, then try to sign back in, it always says my password is wrong when it is not","incorrect password when correct password entered","email","other",NULL,NULL,NULL,NULL,NULL,NULL,"",NULL,NULL,NULL,NULL,"","","",NULL);
+insert into issue VALUES(13, "LGulliver","RBille","inProgress","an error pops up whenever i try to send an email","error on email send","email","cantSend",NULL,NULL,NULL,NULL,NULL,NULL,"",NULL,NULL,NULL,NULL,"","","",NULL);
+insert into issue VALUES(14, "BPrattern",NULL,"new","my colleagues send me emails but I don't receive them. Nothing comes up in my inbox","not receiving emails","email","cantReceive",NULL,NULL,NULL,NULL,NULL,NULL,"",NULL,NULL,NULL,NULL,"","","",NULL);
+insert into issue VALUES(15, "NBrown","RBille","kBase","I'm getting a lot of emails asking for my student username and password login","getting phishing emails","email","spamOrPhishing",NULL,NULL,NULL,NULL,NULL,NULL,"",NULL,NULL,NULL,NULL,"","","",NULL);
+insert into issue VALUES(16, "NBrown","AFaulds","completed","Whenever I sign out of my email address, then try to sign back in, it always says my password is wrong when it is not","incorrect password when correct password entered","email","other",NULL,NULL,NULL,NULL,NULL,NULL,"",NULL,NULL,NULL,NULL,"","","",NULL);
 
 --Account Entries 
-insert into issue VALUES("CDodd","RBille","completed","whenever I try to log in, it says my password is incorrect when Im entering the same password I always use for this login","says password is incorrect when not","account","wrongDetails",NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,"user");
-insert into issue VALUES("BPrattern","AFaulds","inProgress","I've forgotten my password and need to reset it","forgotten password","account","passwordReset",NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,"user");
-insert into issue VALUES("LGulliver",NULL,"new","account says I am a user, when I should be an admin","wrong user status","account","other",NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,"user");
-
-
-insert into room VALUES("ES1.01", 30, true, false, false, true, true, "ES Building", "Computer Lab", "2015-5-18 09:00:00", "2015-5-18 10:00:00");
-insert into room VALUES("ES1.01", 30, true, false, false, true, true, "ES Building", "Computer Lab", "2015-5-18 12:00:00", "2015-5-18 14:00:00");
-insert into room VALUES("ES1.01", 30, true, false, false, true, true, "ES Building", "Computer Lab", "2015-5-18 14:00:00", "2015-5-18 16:00:00");
+insert into issue VALUES(17, "CDodd","RBille","completed","whenever I try to log in, it says my password is incorrect when Im entering the same password I always use for this login","says password is incorrect when not","account","wrongDetails",NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,"user");
+insert into issue VALUES(18, "BPrattern","AFaulds","inProgress","I've forgotten my password and need to reset it","forgotten password","account","passwordReset",NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,"user");
+insert into issue VALUES(19, "LGulliver",NULL,"new","account says I am a user, when I should be an admin","wrong user status","account","other",NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,"user");
---
+++ Example Files/WebAssignment3/WEB-INF/classes/Beans/Issue.java
@@ -0,0 +1,151 @@
+package Beans;
+
+import java.util.Calendar;
+
+public class Issue{
+	
+	private int issueID;
+	private String submitterID, staffID, stateID, body, title, system;
+	private String issueType, subType, location, softwareName, softwareVersion, OS;
+	private boolean restarted, cableConnected, similarIssues;
+	private String hardwareType, compType, compName, emailAddress, browser, accountType;
+	private Calendar timeOccurred;
+	public Issue(){
+		timeOccurred=Calendar.getInstance();
+	}
+	public int getIssueID() {
+		return issueID;
+	}
+	public void setIssueID(int issueID) {
+		this.issueID = issueID;
+	}
+	public String getSubmitterID() {
+		return submitterID;
+	}
+	public void setSubmitterID(String submitterID) {
+		this.submitterID = submitterID;
+	}
+	public String getStaffID() {
+		return staffID;
+	}
+	public void setStaffID(String staffID) {
+		this.staffID = staffID;
+	}
+	public String getStateID() {
+		return stateID;
+	}
+	public void setStateID(String stateID) {
+		this.stateID = stateID;
+	}
+	public String getBody() {
+		return body;
+	}
+	public void setBody(String body) {
+		this.body = body;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getSystem() {
+		return system;
+	}
+	public void setSystem(String system) {
+		this.system = system;
+	}
+	public String getIssueType() {
+		return issueType;
+	}
+	public void setIssueType(String issueType) {
+		this.issueType = issueType;
+	}
+	public String getSubType() {
+		return subType;
+	}
+	public void setSubType(String subType) {
+		this.subType = subType;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getSoftwareName() {
+		return softwareName;
+	}
+	public void setSoftwareName(String softwareName) {
+		this.softwareName = softwareName;
+	}
+	public String getSoftwareVersion() {
+		return softwareVersion;
+	}
+	public void setSoftwareVersion(String softwareVersion) {
+		this.softwareVersion = softwareVersion;
+	}
+	public String getOS() {
+		return OS;
+	}
+	public void setOS(String oS) {
+		OS = oS;
+	}
+	public boolean isRestarted() {
+		return restarted;
+	}
+	public void setRestarted(boolean restarted) {
+		this.restarted = restarted;
+	}
+	public boolean isCableConnected() {
+		return cableConnected;
+	}
+	public void setCableConnected(boolean cableConnected) {
+		this.cableConnected = cableConnected;
+	}
+	public boolean isSimilarIssues() {
+		return similarIssues;
+	}
+	public void setSimilarIssues(boolean similarIssues) {
+		this.similarIssues = similarIssues;
+	}
+	public String getHardwareType() {
+		return hardwareType;
+	}
+	public void setHardwareType(String hardwareType) {
+		this.hardwareType = hardwareType;
+	}
+	public String getCompType() {
+		return compType;
+	}
+	public void setCompType(String compType) {
+		this.compType = compType;
+	}
+	public String getCompName() {
+		return compName;
+	}
+	public void setCompName(String compName) {
+		this.compName = compName;
+	}
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+	public String getBrowser() {
+		return browser;
+	}
+	public void setBrowser(String browser) {
+		this.browser = browser;
+	}
+	public String getAccountType() {
+		return accountType;
+	}
+	public void setAccountType(String accountType) {
+		this.accountType = accountType;
+	}
+	public Calendar getTimeOccurred() {
+		return timeOccurred;
+	}
+}
