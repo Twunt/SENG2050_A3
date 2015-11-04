5c433902a1db56e22c63cf235af3317e3f190807
Terbalt <c3146937@uon.edu.au>
2015-11-04 23:06:41 +1100

    updated home stuff

--- Assignment Files/Home.jsp
+++ Assignment Files/Home.jsp
@@ -1,13 +1,29 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html>
 <html>
 <head>
 	<title>IRS Home Page</title>
-	<link rel="stylesheet" type="text/css" href="homeStyle.css">
+	<link rel="stylesheet" type="text/css" href="headerStyle.css">
 </head>
 <body>
 	<%@include file="header.jsp"%>
+
+	<div id="content">
+		Home.jsp
+	</div>
+
 	<h1>Welcome to the Issue Reporting System.</h1>
-	<h2>If you are here, it means you've been having some sort of technical issue, and need some help sorting it out.</h2>
-	<h2>You can use the above links to navigate around our site to report an issue, see your current issues, or browse our database of archived issues.</h2>
+	<h2>You can use the above links to navigate around our site to report an issue or browse our database of archived issues.</h2>
+	<h2>Below is a list of your issues</h2>
+	<table>
+	<tr><th>Title</th><th>Article link</th></tr>
+	<c:forEach var="article" items="${articles}">
+		<tr>
+			<td><c:out value="${article.title}"/></td>
+			<td><a href='KBArticle?issueID=${article.issueID}'>click here</a></td>
+		</tr>
+	</c:forEach>
+</table>
 </body>
 </html>
--- Assignment Files/HomeController.java
+++ Assignment Files/HomeController.java
@@ -7,9 +7,42 @@ import javax.servlet.annotation.WebServlet;
 @WebServlet(name = "Home", urlPatterns = {"/Home"})
 
 public class Home extends HttpServlet {
+	User thisUser = (User)request.getSession.getAttribute("user");
+	string userID = thisUser.getID();
+	String query = "SELECT title,issueID FROM issue where userID ='"+userID+"'";
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 	throws ServletException, IOException {
-		User thisUser = (User)request.getSession.getAttribute("user");
-		string userRole = thisUser.getRole();
+		request.setAttribute("issues", user.getArticles(query));
+		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/WEB-INF/Home.jsp");
+		dispatcher.forward(request,response);
+
+		// User thisUser = (User)request.getSession.getAttribute("user");
+		// string userRole = thisUser.getRole();
+		// if (userRole.equals("user")){
+		// 	RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/WEB-INF/Home.jsp");
+		// 	dispatcher.forward(request,response);
+		// }
+		// else{
+		// 	RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/WEB-INF/staffHome.jsp");
+		// 	dispatcher.forward(request,response);
+		// }
+	}
+
+	public List<Issue> getArticles(String query) throws SQLException{
+		List<Issue> articles = new LinkedList<>();
+		try(Connection connection = Config.getConnection();
+			Statement statement = connection.createStatement();
+			ResultSet result = statement.executeQuery(query);){
+				while(result.next()){
+					Issue article = new Issue();
+					article.setTitle(result.getString(1));
+					article.setIssueID(result.getInt(2));
+					articles.add(article);
+				}
+		}catch(SQLException e){
+			System.err.println(e.getMessage());
+			System.err.println(e.getStackTrace());
+		}
+		return articles;
 	}
 }
